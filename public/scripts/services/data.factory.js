9304165d9ece45ff614383d433447d8a0d4a2da1
Sebastian Florian <rebardesigndk@gmail.com>
2015-10-16 16:23:46 +0200

    feature (dataFactory) $http get on /api/published
    Checking if data is retrieved corectly

--- public/scripts/services/data.factory.js
+++ public/scripts/services/data.factory.js
@@ -5,8 +5,9 @@
 		.module('app')
 		.factory('dataFactory', dataFactory);
 
-	dataFactory.$inject = ['dependency1'];
-	function dataFactory(dependency1) {
+	dataFactory.$inject = ['$log' , '$http'];
+	function dataFactory($log , $http) {
+		
 		var factory = {
 			getPublished:getPublished,
 			getReached:getReached
@@ -15,9 +16,24 @@
 		return factory;
 
 		////////////////
-		function getPublished() { }
+		function getPublished() {
+			
+			return $http.get('/api/published')
+				.then(getPublishedComplete)
+				.catch(getPublishedFailed);
+	
+			function getPublishedComplete(response) {
+				$log.log(response);
+				return response;
+			}
+	
+			function getPublishedFailed(error) {
+				$log.error('ERROR::getPublished Failed ' + error.data);
+			}
+			
 		}
 	
-		function getReached() { }
+		function getReached() {
 		}
+	}
 })();
