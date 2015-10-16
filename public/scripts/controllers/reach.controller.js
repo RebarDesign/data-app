7f92212f332ef40be6712805bc09c516f189036a
Sebastian Florian <rebardesigndk@gmail.com>
2015-10-16 19:49:40 +0200

    chore (reached.html ) Create svg element, with id of #bar-chart

--- public/scripts/controllers/reach.controller.js
+++ public/scripts/controllers/reach.controller.js
@@ -100,6 +100,28 @@
 			//* fancy-debugging *//
 			// debugger
 			
+			var x = d3.scale.ordinal()
+				.rangeRoundBands([0, width], .1);
+			
+			var y = d3.scale.linear()
+				.range([height, 0]);
+			
+			var xAxis = d3.svg.axis()
+				.scale(x)
+				.orient("bottom");
+			
+			var yAxis = d3.svg.axis()
+				.scale(y)
+				.orient("left")
+				.ticks(10);
+				
+			var svg = d3.select("#chart2")
+				.attr("width", width + margin.left + margin.right)
+				.attr("height", height + margin.top + margin.bottom)
+				.append("g")
+				.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+			
+			
 		}
 	}
 })();
