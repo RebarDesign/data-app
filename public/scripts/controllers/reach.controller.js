28fd1fc0ae67ed680010ac25cc88b0a40ce6770d
Sebastian Florian <rebardesigndk@gmail.com>
2015-10-18 17:13:46 +0200

    refractor ( ReachController ) Remove initial chart function

--- public/scripts/controllers/reach.controller.js
+++ public/scripts/controllers/reach.controller.js
@@ -57,7 +57,6 @@
 				vm.reachData = cleanArray(vm.reachData);
 				//* ghetto-debugging *//
 				// $log.info('OK:: getReachData(): ', vm.reachData);
-				// drawBars(vm.reachData);
 				drawStackedBars(vm.reachData);
 			})
 		
@@ -114,111 +113,6 @@
 			// $log.log('Cleaned ', newArray);
 			
 			return newArray;
-		}
-		
-		function drawBars(array){
-			
-			// find our element and append size it
-			var svg = d3.select("#bar-chart")
-				.attr("width", width + margin.left + margin.right)
-				.attr("height", height + margin.top + margin.bottom)
-				.append("g")
-				.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-			
-			// get highest post value
-			var yMax = d3.max(array, function(d){ return Math.max(d.total); });
-			
-			//* fancy-debugging *//
-			// debugger
-				
-			// set x domain the number of elements
-			x.domain(array.map(function(d) { return d.index; }));
-			
-			// set y domain, the highest post value
-			y.domain([0, yMax]);
-			
-			// style the x axis
-			svg.append("g")
-				.attr("class", "x axis")
-				.attr("transform", "translate(0," + height + ")")
-				.attr("font-size", "7px")
-				.call(xAxis);
-			
-			// x axis label
-			svg.append("text")
-				.attr("class", "x axis")
-				.attr("text-anchor", "end")
-				.attr("x", width / 2)
-				.attr("y", height+ 25)
-				.text("Post Item");
-				
-			//TODO Style the label 
-				
-				
-			// style the y axis
-			svg.append("g")
-				.attr("class", "y axis")
-				.call(yAxis)
-				.append("text")
-				.attr("transform", "rotate(-90)")
-				.attr("y", 6)
-				.attr("dy", ".71em")
-				.style("text-anchor", "end")
-				.text("Impressions");
-				
-			// draw the bars
-			svg.selectAll(".bar")
-				.data(array)
-				.enter().append("rect")
-				.attr("class", "bar")
-				.attr("x", function(d) { return x(d.index); })
-				.attr("width", x.rangeBand())
-				.attr("y", function(d) { return y(d.total); })
-				.attr("height", function(d) { return height - y(d.total); })
-				// info on hover
-				.append("svg:title")
-   				.text(function(d) { return 'Post ' + d.index + ' : ' + d.total + ' at ' + parseDate(d.timestamp);});
-			
-			// monitor checkbox
-			d3.select("input").on("change", change);
-			
-			function change() {
-				
-				
-				// // delay the redraw of the elements
-				// var sortTimeout = setTimeout(function() {
-				// 	d3.select("input").property("checked", true).each(change);
-				// }, 100);
-				
-				// clearTimeout(sortTimeout);
-				
-				// Copy-on-write since tweens are evaluated after a delay.
-				// order either by post number or index
-				var x0 = x.domain(array.sort(this.checked
-					? function(a, b) { return b.total - a.total; }
-					: function(a, b) { return d3.ascending(a.index, b.index); })
-					.map(function(d) { return d.index; }))
-					.copy();
-					
-				// sort by index
-				// they can be sorted by timestamp, but it's the same
-				svg.selectAll(".bar")
-					.sort(function(a, b) { return x0(a.index) - x0(b.index); });
-					
-				// make relay relative to element index
-				var transition = svg.transition().duration(750),
-					delay = function(d, i) { return i * 5; };
-					
-				
-				transition.selectAll(".bar")
-					.delay(delay)
-					.attr("x", function(d) { return x0(d.index); });
-					
-				transition.select(".x.axis")
-					.call(xAxis)
-					.selectAll("g")
-					.delay(delay);
-			}
 		}
 		
 		function drawStackedBars(array){
