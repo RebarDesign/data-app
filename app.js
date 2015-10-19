98a69c099940233a701041468d6761d665cd2652
unknown <rebardesigndk@gmail.com>
2015-10-19 19:04:00 +0200

    feature (app.js) Comment out server logger.

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'html');
 
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+// app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
