9e8b7825d78484146456d2a51f6075d60977be56
Sebastian Florian <rebardesigndk@gmail.com>
2015-11-01 20:08:45 +0100

    feature ( API ) Extend "publish" API

--- routes/api.js
+++ routes/api.js
@@ -4,13 +4,33 @@ var router = express.Router();
 var publishData = require('./assets/publish.json');
 var reachData = require('./assets/reach.json');
 
-router.get('/published', function (req, res) {
-  res.json(publishData);
-});
+router.route('/published')
+  .get(function (req, res) {
+    res.json(publishData);
+  })
+  .post(function(req, res) {
+          // TODO change publish data in file
+          publishData.push(req.body);
+          res.json({responde: 'Published item created', status:"200"});
+  })
+  
+router.route('/published/:publish_id')
+  .get(function(req, res) {
+          var publishId = req.params.publish_id;
+          var response = [];
+          var i;
+          
+          for(i = 0; i < publishData.length; i++){
+              if(publishData[i].id == publishId) {
+                  response = publishData[i];
+              }
+          }
+          res.json({res:response, status: "200"});
+  });
 
-router.get('/reach', function (req, res) {
-  res.json(reachData);
-});
-
+router.route('/reach')
+  .get(function (req, res) {
+    res.json(reachData);
+  });
 
 module.exports = router;
